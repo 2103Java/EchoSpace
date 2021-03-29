709aecb11a10c957b0c160c5c03ac70f338e6800
hassanshallal <hshallal@icloud.com>
2021-03-29 14:16:08 -0600

    hshallal

---
+++ HshallalWeakEcho.java
@@ -0,0 +1,12 @@
+public class WeakEcho{
+
+    public static void main(String[] args){
+        
+        for(String s: args){
+            System.out.print(s + " ");
+            
+        }
+
+
+    }
+}
