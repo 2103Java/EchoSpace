8a77694d3b07c6b0272a5312055aeaa3654e9ffa
hassanshallal <hshallal@icloud.com>
2021-03-30 10:34:20 -0600

    branching

--- HshallalWeakEcho.java
+++ HshallalWeakEcho.java
@@ -3,7 +3,7 @@ public class WeakEcho{
     public static void main(String[] args){
         
         for(String s: args){
-            System.out.print(s + " ");
+            System.out.print(s + "_");
             
         }
 
